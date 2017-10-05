646845593e85c6c8562b1b057b5153ff3eab5f07
jik <jik@LAPTOP-JIK2.oxia.corp>
2017-10-05 22:10:38 +0100

    First Commit from Eclipse

---
+++ src/main/java/com/jkaouech/main/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jkaouech.main;
+
+/**
+ * @author Jihed KAOUECH
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD !");
+
+	}
+
+}
